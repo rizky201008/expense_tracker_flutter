16cd7b56e1177f9da7bd893c700ce9279fdac922
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-11-08 08:54:38 +0700

    update: lock orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(
-      cardTheme: const CardTheme(
-        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      ),
-      useMaterial3: true,
-      colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF8000)),
-    ),
-    home: const Expenses(),
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (value) {
+      runApp(MaterialApp(
+        theme: ThemeData(
+          cardTheme: const CardTheme(
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF8000)),
+        ),
+        home: const Expenses(),
+      ));
+    },
+  );
 }
